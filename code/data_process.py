eb69900bdfacedd170ad7064a33345b141b34e96
lingchuanhu <lingchuanhu@tencent.com>
2018-09-19 13:07:27 +0800

    init

--- code/data_process.py
+++ code/data_process.py
@@ -2,6 +2,11 @@
 
 import pandas as pd
 
-basic_info = pd.read_csv('./data/train/企业基本信息1.csv')
+basic_info = pd.read_csv('../data/train/企业基本信息1.csv')
+qian_shui_ming_dan = pd.read_csv('../data/train/欠税名单5.csv')
 
-basic_info.sample()
+# print(basic_info.sample())
+#
+# print(basic_info.小微企业ID.unique)
+
+print(qian_shui_ming_dan.所欠税种.unique())
