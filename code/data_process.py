f4197e7f2273dc8061778065eae438ad1a61044e
LingchuanHu <hulingchuan@hotmail.com>
2018-09-19 22:07:58 +0800

    more

--- code/data_process.py
+++ code/data_process.py
@@ -1,12 +1,20 @@
 # -*- coding: utf-8 -*-
 
 import pandas as pd
+import matplotlib.pyplot as plt
 
-basic_info = pd.read_csv('../data/train/企业基本信息1.csv')
-qian_shui_ming_dan = pd.read_csv('../data/train/欠税名单5.csv')
+from matplotlib import font_manager
 
-# print(basic_info.sample())
-#
-# print(basic_info.小微企业ID.unique)
+basic_info = pd.read_csv('../data/train/企业基本信息1.csv', keep_default_na=False, na_values=[""])
+qian_shui_ming_dan = pd.read_csv('../data/train/欠税名单5.csv', keep_default_na=False, na_values=[""])
+min_shang_shi_cai_pan_wen_shu = pd.read_csv('../data/train/民商事裁判文书2.csv', keep_default_na=False, na_values=[""])
 
-print(qian_shui_ming_dan.所欠税种.unique())
+# 画出来税种的类型，未解决问题中文没显示出来
+x = qian_shui_ming_dan.groupby('所欠税种')['小微企业ID'].nunique()
+y = x.plot(kind='bar')
+plt.show(block=True)
+
+# 将数据合并到一个二维数组里面
+print(basic_info)
+basic_info = pd.merge(left=basic_info, right=qian_shui_ming_dan, how='left', left_on='小微企业ID', right_on='小微企业ID')
+print(basic_info)
