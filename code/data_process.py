67d4a30217ee22e6e5ba84541b150110ed0ff50f
Liben <liben@iie.ac.cn>
2018-09-22 14:39:13 +0800

    b

--- code/data_process.py
+++ code/data_process.py
@@ -4,6 +4,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from code.lib.currency_exchange import currency_exchange
 from code.lib.utils import normalize
+import seaborn as sns
 
 import matplotlib
 matplotlib.rcParams['font.sans-serif'] = 'SimHei'
@@ -13,22 +14,51 @@ def ji_ben_xin_xi():
                                      keep_default_na=False,
                                      na_values=[""])
 
+
+    #将老赖的信息并入基本信息中
     lao_lai = pd.read_csv('../data/train/8.csv',
                                      keep_default_na=False,
                                      na_values=[""])
-
-    lao_lai.insert(1,'是否老赖','是')
-
-    for index,row in lao_lai.iterrows():
-        x = data_ji_ben_xin_xi.loc[data_ji_ben_xin_xi['小微企业ID'] == row['小微企业ID']]
-
+    lao_lai.insert(1,'是否老赖',1)
     outfile = pd.merge(data_ji_ben_xin_xi, lao_lai, how='left', left_on='小微企业ID', right_on='小微企业ID')
+    def clear_Nan(s):
+        if pd.isna(s):
+            return 0
+        else:
+            return 1
+    outfile['是否老赖'] = outfile['是否老赖'].apply(clear_Nan)
+
+
+    #分析注册资金
+    def money_label(s):
+        if (s <= 1000):
+            return 1
+        elif ((s > 1000) & (s <= 5000)):
+            return 2
+        elif ((s > 5000) & (s <= 10000)):
+            return 3
+        elif ((s > 10000) & (s <= 20000)):
+            return 4
+        elif ((s > 20000) & (s <= 30000)):
+            return 5
+        elif (s > 30000):
+            return 6
+    outfile['注册资金分段'] = outfile['注册资金(万元)'].apply(money_label)
+    #sns.barplot(x="注册资金分段", y="是否老赖", data=outfile, palette='Set3')
+    plt.show()
+
+    #分析注册资金类型
+    #sns.barplot(x="注册资本(金)币种", y="是否老赖", data=outfile, palette='Set3')
+    plt.show()
+
+    sns.barplot(x="企业(机构)类型", y="是否老赖", data=outfile, palette='Set3')
+    plt.show()
+
+    print(data_ji_ben_xin_xi.head())
+
     outfile.to_csv('outfile.csv', index=False)
 
-
-
-    print(outfile)
-
+    print(data_ji_ben_xin_xi['企业(机构)类型'].unique())
 
     exit(1)
 
