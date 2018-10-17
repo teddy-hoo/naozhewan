26cc8f61535f0441d1ab544e417db9dbbf162ca0
lingchuanhu <lingchuanhu@tencent.com>
2018-10-17 16:59:17 +0800

    imbalanced优化尝试

--- code/gdbt.py
+++ code/gdbt.py
@@ -18,9 +18,7 @@ pd.options.display.max_columns = 1000
 def gdbt():
 
     df = pd.read_csv('../data/processed/train_data.csv')
-    print(df.values.shape)
 
-    print(df.values)
     X = np.asarray(df.values[:,2:-1], np.float32)
     Y = np.asarray(df.values[:,-1], np.float32)
 
@@ -28,19 +26,42 @@ def gdbt():
 
     X_train, X_test, y_train, y_test = train_test_split(X, Y, random_state=432)
 
-    clf = GradientBoostingClassifier(n_estimators=10000, learning_rate=0.01,
-            max_depth=1, random_state=432)
-    clf.fit(X_train, y_train)
+    x = pd.read_csv('../data/processed/train_data.csv')
+    data = x.drop(columns=['小微企业ID']).values
 
-    y_pred = clf.predict(X_test)
-    print(y_pred[:100])
-    print(y_test[:100])
-    print(accuracy_score(y_test, y_pred))
-    fpr, tpr, thresholds = metrics.roc_curve(y_test, y_pred)
+    msk = np.random.random(len(data)) < 0.8
+
+    train_data = data[msk]
+    eval_data = data[~msk]
+    class_1 = train_data[np.where(train_data[:, -1] == 1)[0]]
+    # 调整class为1的样本量来解决样本量不平均的问题
+    for i in range(10):
+        train_data = np.concatenate((train_data, class_1), axis=0)
+    np.random.shuffle(train_data)
+
+    train_label = np.asarray(train_data[:, -1], np.float32)
+    train_data = np.asarray(train_data[:, 1:-1], np.float32)
+    train_data = preprocessing.scale(train_data)
+    # print("train: ", train_data)
+    # print("X: ", X_train)
+    # print("train: ", train_label)
+    # print("y: ", y_train[:1000])
+    # exit()
+
+    eval_label = np.asarray(eval_data[:, -1], np.float32)
+    eval_data = np.asarray(eval_data[:, 1:-1], np.float32)
+
+    clf = GradientBoostingClassifier(n_estimators=1000, learning_rate=0.01,
+            max_depth=1, random_state=432)
+    clf.fit(train_data, train_label)
+
+    y_pred = clf.predict(eval_data)
+    print("Text Acc: ", accuracy_score(eval_label, y_pred))
+    fpr, tpr, thresholds = metrics.roc_curve(eval_label, y_pred)
     auc = metrics.auc(fpr, tpr)
     print("Test Auc: ", auc)
     plot_roc(fpr, tpr, auc)
-    cm = metrics.confusion_matrix(y_test, y_pred)
+    cm = metrics.confusion_matrix(eval_label, y_pred)
     sns.heatmap(cm, annot=True, fmt="d")
     plt.show()
 
--- code/nn.py
+++ code/nn.py
@@ -12,7 +12,11 @@ import matplotlib.pyplot as plt
 
 def build_model(init_size):
     model = keras.Sequential()
+    model.add(keras.layers.Dense(32, activation='relu', input_shape=(init_size, )))
+    model.add(keras.layers.Dropout(0.5))
     model.add(keras.layers.Dense(64, activation='relu', input_shape=(init_size, )))
+    model.add(keras.layers.Dropout(0.5))
+    model.add(keras.layers.Dense(64, activation='relu'))
     model.add(keras.layers.Dropout(0.5))
     model.add(keras.layers.Dense(32, activation='relu'))
     model.add(keras.layers.Dropout(0.5))
@@ -20,7 +24,7 @@ def build_model(init_size):
 
     model.add(keras.layers.Dense(2, activation=tf.nn.sigmoid))
 
-    model.compile(optimizer=tf.train.AdamOptimizer(0.001),
+    model.compile(optimizer=tf.train.AdamOptimizer(0.01),
                   loss='categorical_crossentropy',
                   metrics=['accuracy'])
     return model
@@ -32,62 +36,35 @@ EPOCHS = 20
 def nn():
     # data, label = get_train_data()
     x = pd.read_csv('../data/processed/train_data.csv')
+    data = normalize(x.drop(columns=['小微企业ID', '是否老赖']).values)
 
-    drop_list = [
-    '行业门类代码',
-    '投资总额(万元)',
-    '欠税税务机关',
-    '行政处罚次数',
-    '处罚机关_市场',
-    '处罚机关_邮政',
-    '审理机关_6_x',
-    '审理机关_7_x',
-    '文书类型_3.0',
-    '文书类型_8.0',
-    '诉讼地位_10',
-    '审理机关_0_x',
-    '诉讼地位_15',
-    '审理程序_1',
-    '审理机关_1_x',
-    '审理机关_2_x',
-    '公告类型_1',
-    '公告类型_2',
-    '公告类型_3',
-    '公告类型_12',
-    '公告类型_13',
-    '公告类型_14',
-    '公告类型_15',
-    '公告类型_16',
-    '公告类型_17',
-    '公告类型_18',
-    '公告类型_19',
-    '公告类型_21',
-    '公告类型_22',
-    '公告类型_23',
-    '公告类型_25',
-    '诉讼地位3_6',
-    '诉讼地位3_7'
-    ]
-    # x = x.drop(drop_list, axis=1)  # do not modify x, we will use it later
-
-    label = pd.get_dummies(x['是否老赖']).values
-    data = x.drop(columns=['小微企业ID', '是否老赖'])
-    data = normalize(data.values)
+    data = np.concatenate((data, pd.get_dummies(x['是否老赖']).values), axis=1)
 
     msk = np.random.random(len(data)) < 0.8
-    train_label = label[msk]
+
     train_data = data[msk]
-
-    eval_label = label[~msk]
     eval_data = data[~msk]
+    class_1 = train_data[np.where(train_data[:, -1] == 1)[0]]
+    # 调整class为1的样本量来解决样本量不平均的问题
+    for i in range(20):
+        train_data = np.concatenate((train_data, class_1), axis=0)
+    np.random.shuffle(train_data)
 
-    print(train_data.shape, eval_data.shape)
+    train_label = train_data[:, -2:]
+    train_data = train_data[:, 1:-2]
+
+    eval_label = eval_data[:, -2:]
+    eval_data = eval_data[:, 1:-2]
+
+    print(train_data.shape, train_label.shape, eval_data.shape, eval_label.shape)
 
     model = build_model(train_data.shape[1])
     model.summary()
 
     history = model.fit(train_data, train_label, epochs=EPOCHS, batch_size=100,
-                        validation_split=0.2, verbose=0)
+                        validation_split=0.2, verbose=0,
+                        # 调整loss-weight来解决样本量不平均的问题
+                        class_weight={0: 1, 1: 1})
     plot_history(history)
 
     test_loss, test_acc = model.evaluate(eval_data, eval_label)
@@ -103,7 +80,7 @@ def nn():
     # print(tpr)
     auc = metrics.auc(fpr, tpr)
     print("Test Auc: ", auc)
-    # plot_roc(fpr, tpr, auc)
+    plot_roc(fpr, tpr, auc)
 
     print(np.count_nonzero(eval_label[:, -1]))
     print(np.count_nonzero(np.argmax(predictions, axis=1)))
